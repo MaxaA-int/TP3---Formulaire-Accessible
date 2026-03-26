b4089140692f220028b8f2b8f8facab942ceca99
Maxime Riverin <maximeriverin07@gmail.com>
2026-03-26 10:31:43 -0400

    Update index.html & style.css
    Stylisation des messages d'erreur pour table et mobile
    Input select sélectionne directement une option

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.getElementById('pays').addEventListener('blur', validerPays);
+document.getElementById('province_etat').addEventListener('blur', validerProvinceEtat);
+
+function validerPays() {
+
+}
+
+function validerProvinceEtat() {
+
+}
